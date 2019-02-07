70fe07cc6274c53362df3f5e32d86f6c15954740
Joe Berkem <jberkem@gmail.com>
2019-02-07 10:23:14 +0000

    gulpfile completed and checked

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
 let rename = require('gulp-rename');
 let sass = require ('gulp-sass');
-// browser-sync .create()
+// let watch = require('gulp-watch');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglify-es').default;
 
 
 gulp.task('sass', () => {
 	return gulp.src('./scss/styles.scss')
 	.pipe(sass()) 
-	.pipe(gulp.dest('./dist/')) //./dist/ es una carpeta con archivos "resultado", esos que yo nunca voy 
-	//a tener la necesidada de editar y que son los que le voy a dar a mi cliente (?). 
-
+	.pipe(gulp.dest('./css/')) 
 	.pipe(rename('styles.css'));
 });
 
@@ -25,28 +25,16 @@ gulp.task('styles', gulp.series('sass', 'minify-css'));
 
 gulp.task('watch', () => {
 	gulp.watch('./scss/*.scss', gulp.series('styles'));
-
+	gulp.watch('./js/*.js', gulp.series('javascript'));
 });
 
-////////////////////////////////////////
+gulp.task('javascript', () => {
+   return gulp.src('./js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('./dist/'));
+});
 
-// let concat = require('gulp-concat');
-// let uglify = require('gulp-uglify-es').default;
-
-// gulp.task('concat', () => {
-// 	return gulp.src('js/*.js') //cuidado con el orden de los archivos, porque todos dependen del jquery 
-// 	//(éste necesita ir primero en el caso de declarar los arhivos explícitamente: ('js/jquery*.js', 'js/popper.js', 'bootstrap.js', 'js/*.js')
-// 	.pipe(concat('all.js'))
-// 	.pipe(gulp.dest('./'))
-// });
-
-// gulp.task('uglify', () => {
-// 	return gulp.src('all.js')
-// 	// .pipe(rename({suffix: '.min'}))
-// 	.pipe(uglify(	))
-// 	.pipe(gulp.dest('./'));
-// });
-
-// gulp.task('js', gulp.series('concat', 'uglify'));
 
 
---
+++ js/main.js
@@ -0,0 +1,1 @@
+console.log("changed for watch")
